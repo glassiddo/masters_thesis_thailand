601e7d55d183936efeeaf29fbed3b867966641c5
glassiddo <iddo.glass@ehess.fr>
2026-04-15 11:57:29 +0200

    clean read fires file

--- do/prepare/2a_read fires.R
+++ do/prepare/2a_read fires.R
@@ -1,22 +1,14 @@
-source("code/setup.R")
+source("do/setup.R")
 
 ##### load countries and create function to filter fires and match to admin units ----
 all <- read_sf(here(build.dir, "units", "adm1_units_detailed.shp")) 
 
-myanmar <- all |> 
-  filter(ADM0_EN == "Myanmar") |> 
+admin_units <- read_sf(here(build.dir, "units", "adm1_units_detailed.shp")) |> 
   select(adm0_id, adm1_id)
 
-thailand <- all |> 
-  filter(ADM0_EN == "Thailand") |> 
-  select(adm0_id, adm1_id)
-
-laos <- all |> 
-  filter(ADM0_EN == "Laos") |> 
-  select(adm0_id, adm1_id)
-
-process_fires <- function(data_path, units) {
-  read_sf(data_path) |>
+process_fires <- function(data_paths, units) {
+  data_paths |> 
+    map_df(~read_sf(.x)) |>
     rename(date = ACQ_DATE) |> 
     mutate(month = month(date), year = year(date)) |>
     select(-c("INSTRUMENT", "VERSION")) |>
@@ -31,54 +23,40 @@ process_fires <- function(data_path, units) {
 
 ### modis ----
 # were downloaded as different per-country file from the nasa website
-modis_thai <- process_fires(
- here(raw.dir, "fires", "thailand", "modis", "fire_archive_M-C61_620555.shp"), thailand
-  )
-
-modis_myan <- process_fires(
-  here(raw.dir, "fires", "myanmar", "modis", "fire_archive_M-C61_620556.shp"), myanmar
-  )
-
-modis_laos <- process_fires(
-  here(raw.dir, "fires", "laos", "modis", "fire_archive_M-C61_627751.shp"), laos
+files <- list(
+  modis = here(raw.dir, "fires", 
+               c("thailand/modis/fire_archive_M-C61_620555.shp",
+                 "myanmar/modis/fire_archive_M-C61_620556.shp",
+                 "laos/modis/fire_archive_M-C61_627751.shp")),
+  
+  viirs = here(raw.dir, "fires", 
+               c("thailand/viirs/fire_archive_SV-C2_629651.shp",
+                 "myanmar/viirs/fire_archive_SV-C2_629653.shp",
+                 "laos/viirs/fire_archive_SV-C2_629652.shp"))
 )
 
-modis_fires <- bind_rows(modis_thai, modis_myan, modis_laos) |>
-  mutate(
-    fire_id = row_number() # give unique identifier for each fire
-  )
+# process and give unique identifier for each fire
+# (must happen after merging otherwise ids will be repeated)
+modis_fires <- process_fires(files$modis, admin_units) |> 
+  mutate(fire_id = row_number()) 
 
-st_write(modis_fires, here(out.dir, "modis_with_region_2206.gpkg"), append = FALSE)
+st_write(modis_fires, here(build.dir, "modis_with_region_2206.gpkg"), delete_dsn = TRUE)
 
 # only keep fire id and date for the file uploaded to gee
 modis_for_gee <- modis_fires |> select(fire_id, date)
 
-st_write(modis_for_gee, "fires for gee/modis_with_region_for_gee_1606.shp", append = FALSE)
+st_write(modis_for_gee, here(build.dir, "fires for gee", "modis_with_region_for_gee_1606.shp"), append = FALSE)
 
 # test for duplicates
 modis_fires |> 
   st_drop_geometry() |> 
   janitor::get_dupes(fire_id)
 
+rm(modis_fires, modis_for_gee)
+gc()
 
-### viirs -----
+viirs_fires <- process_fires(files$viirs, admin_units) |> 
+  mutate(fire_id = row_number())
 
-viirs_thai <- process_fires(
-  here(raw.dir, "fires", "thailand", "viirs", "fire_archive_SV-C2_629651.shp"), thailand
-) 
-
-viirs_myan <- process_fires(
-  here(raw.dir, "fires", "myanmar", "viirs", "fire_archive_SV-C2_629653.shp"), myanmar
-) 
-
-viirs_laos <- process_fires(
-  here(raw.dir, "fires", "laos", "viirs", "fire_archive_SV-C2_629652.shp"), laos
-) 
-
-viirs_fires <- bind_rows(viirs_thai, viirs_myan, viirs_laos) |> 
-  mutate(
-    fire_id = row_number() # give unique identifier for each fire
-  ) 
-
-st_write(viirs_fires, here(build.dir, "viirs_with_region_2906.gpkg"), append = FALSE)
+st_write(viirs_fires, here(build.dir, "viirs_with_region_2906.gpkg"), delete_dsn = TRUE)
 
